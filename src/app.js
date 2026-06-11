549cc6281b4716759af25b51eb66be9637bb6b3f
javimateo <javimateo2003@gmail.com>
2026-06-11 19:26:44 +0200

    feat: fase 3 - API de autores (GET list, GET by id, POST)

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/api/authors', require('./routes/authors.routes'));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
